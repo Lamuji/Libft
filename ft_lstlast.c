baec59bdf4ad2374059e8a5a50a6c3fa50ff49af
Ramzi Fkaier <rfkaier@c1r6p2.42nice.fr>
2021-04-10 19:54:12 +0200

    ready to 115

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hkrifa <hkrifa@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rfkaier <rfkaier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 16:47:23 by hkrifa            #+#    #+#             */
-/*   Updated: 2021/04/08 13:51:06 by hkrifa           ###   ########.fr       */
+/*   Updated: 2021/04/10 19:48:51 by rfkaier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstlast(t_list *lst)
 {
 	if (lst == NULL)
 		return (lst);
-	while (lst -> next != NULL)
+	while (lst->next != NULL)
 		lst = lst->next;
 	return (lst);
 }
